c399f91a579b71080fe608ac7a520371f89288df
Pranit <dev.pranitpatil@gmail.com>
2022-09-23 13:55:29 +0530

    edit in user model and created api endpoint for adding assignment

--- model/User.js
+++ model/User.js
@@ -4,7 +4,10 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true, default: "" },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    teacher: { type: Boolean, required: true, default: false },
+    teacher: { type: Boolean, required: true, default: false},
+    prn: {type:Number},
+    branch: {type: String},
+    year: {type: Number}
 }, { timestamps: true })
 
 mongoose.models = {}
