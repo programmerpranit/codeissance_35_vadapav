1868790470ee6b97d45c9247b5f23242a4db86a2
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2022-09-23 18:19:29 +0530

    teachers profile created

---
+++ pages/teacher/profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const profile = () => {
+  return (
+    <>
+      <div className=" rounded-md  m-4 p-6 border-black border-2 w-min">
+        <h1 className="p-3 m-1 text-3xl border-b-2 border-black">
+          Teacher's Name
+        </h1>
+        <h1 className="p-3 m-1 text-lg border-black border-b">Email Address</h1>
+      </div>
+    </>
+  );
+};
+
+export default profile;
