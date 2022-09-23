b32e0a79e7f7fedf17e01b2701b9ecc4f7cf9af9
Pranit <dev.pranitpatil@gmail.com>
2022-09-23 16:46:51 +0530

    psp

--- pages/bulkupload.jsx
+++ pages/bulkupload.jsx
@@ -1,60 +1,81 @@
 import React, { useState } from "react";
-
+import Papa from "papaparse";
+ 
+// Allowed extensions for input file
+const allowedExtensions = ["csv"];
+ 
 const BulkUpload = () => {
-
+     
+    // This state will store the parsed data
+    const [data, setData] = useState([]);
+     
+    // It state will contain the error when
+    // correct file extension is not used
+    const [error, setError] = useState("");
+     
+    // It will store the file uploaded by the user
     const [file, setFile] = useState("");
-
-    const [array, setArray] = useState([])
-
-
-    
-
-    const handleUpload = (delimiter = ",") => {
-
-        console.log(file)
-
-        let reader = new FileReader();
-
-        reader.onload = function (e) {
-            
-            let str = reader.readAsText(file);
-            
-            let headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-            
-            // slice from \n index + 1 to the end of the text
-            // use split to create an array of each csv value row
-            let rows = str.slice(str.indexOf("\n") + 1).split("\n");
-            
-            let arr = rows.map(function (row) {
-            const values = row.split(delimiter);
-            const el = headers.reduce(function (object, header, index) {
-              object[header] = values[index];
-              return object;
-            }, {});
-            return el;
-        });
-        
-        console.log(arr)
-        
+ 
+    // This function will be called when
+    // the file input changes
+    const handleFileChange = (e) => {
+        setError("");
+         
+        // Check if user has entered the file
+        if (e.target.files.length) {
+            const inputFile = e.target.files[0];
+             
+            // Check the file extensions, if it not
+            // included in the allowed extensions
+            // we show the error
+            const fileExtension = inputFile?.type.split("/")[1];
+            if (!allowedExtensions.includes(fileExtension)) {
+                setError("Please input a csv file");
+                return;
+            }
+ 
+            // If input type is correct set the state
+            setFile(inputFile);
+        }
     };
-        //   setArray(arr)
-
-    }
-
-
-  return (
-    <>
-      <input
-              type="file"
-              name="csvfile"
-              accept=".csv"
-              className="mt-2"
-              onChange={(e) => setFile(e.target.files[0])}
+    const handleParse = () => {
+         
+        // If user clicks the parse button without
+        // a file we show a error
+        if (!file) return setError("Enter a valid file");
+ 
+        // Initialize a reader which allows user
+        // to read any file or blob.
+        const reader = new FileReader();
+         
+        // Event listener on reader when the file
+        // loads, we parse it and set the data.
+        reader.onload = async ({ target }) => {
+            const csv = Papa.parse(target.result, { header: true });
+            const parsedData = csv?.data;
+            const columns = Object.keys(parsedData[0]);
+            console.log(parsedData)
+            setData(columns);
+        };
+        reader.readAsText(file);
+    };
+ 
+    return (
+        <div>
+            <label htmlFor="csvInput" style={{ display: "block" }}>
+                Enter CSV File
+            </label>
+            <input
+                onChange={handleFileChange}
+                id="csvInput"
+                name="file"
+                type="file"
             />
-
-            <button onClick={handleUpload}>Upload</button>
-    </>
-  );
+            <div className="my-5"> 
+                <button onClick={handleParse}>Parse</button>
+            </div>
+        </div>
+    );
 };
-
+ 
 export default BulkUpload;
