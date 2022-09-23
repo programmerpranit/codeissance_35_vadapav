91eda4b8497d9dc49a67a07d169219b3af0d5180
Pranit <dev.pranitpatil@gmail.com>
2022-09-23 18:48:01 +0530

    apis working

--- pages/api/assignment/[id].js
+++ pages/api/assignment/[id].js
@@ -1,7 +1,6 @@
-
 import connectDb from "../../../middleware/mongoose";
 import Assignment from "../../../model/Assignment";
-
+import jwt from "jsonwebtoken"
 
 const handler = async (req, res) => {
 
@@ -9,25 +8,30 @@ const handler = async (req, res) => {
 
     const { token, fileUrl } = req.body;
     const user = jwt.decode(token, "jwtsecret");
+    if(user){
 
-    try {
         
-        let assignment = await Assignment.findById(id)
-        assignment.submissions.push({
+        try {
+            
+            let assignment = await Assignment.findById(id)
+            assignment.submissions.push({
             studentName: user.name,
-            studentId: user._id,
+            studentId: user.id,
             date: Date.now(),
             fileUrl: fileUrl
         })
-
-        assignment.save();
-
+        
+        await assignment.save();
+        
         return res.status(201).json({ message: "Assignment Submitted Successfully" });
-
+        
     } catch (error) {
         return res.status(500).json({message: error});
     }
-
+    
+} else {
+    return res.status(500).json({message: "Invalid Token"});
+}
     
 
 }
