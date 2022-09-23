ba85fe81afd6f540418e4eeeacf18a7e891f0631
rane_sarvesh <ranesarvesh97@gmail.com>
2022-09-23 12:34:47 +0530

    user and notice schema added

---
+++ model/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, default: "" },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    teacher: { type: Boolean, required: true, default: false },
+}, { timestamps: true })
+
+mongoose.models = {}
+
+export default mongoose.model("User", UserSchema);
