1d6c49e3b72af5a3e673b437b067a23d17634f2e
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-24 08:47:41 +0530

    updating hovers of buttons

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ export default function Home() {
     <>
       <div className="flex-col content-center ">
         <h1 className="text-5xl font-bold text-center  border-b-2 border-black p-4 m-2">
-          Nikaaal!
+          Nikaaal
         </h1>
-        <p className="text-6xl p-8 m-2 mt-20  border-2 rounded-md border-black font-sans text-center font-bold ">
+        <p className="text-6xl p-8 m-2 mt-20  border-2 rounded-md  border-black font-sans text-center font-bold ">
         The ultimate destination for managing student reports, assignments and more!
 
         </p>
         <div className="flex justify-center mt-20">
           <Link href={"/account/signup"}>
-            <button className="text-lg hover:bg-blue-300  p-4 rounded-md m-2 text-black border-2 border-black">
+            <button className="text-lg hover:bg-gray-400  p-4 rounded-md m-2 text-black border-2 border-black">
               Sign Up!
             </button>
           </Link>
